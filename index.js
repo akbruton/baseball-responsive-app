47802d88732cb6b593d0e7623f26358def86cde6
akbruton <brutonak@gmail.com>
2016-07-12 09:32:28 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function learnMore() {
+    alert("Here is some more information.");
+}
+
+function moreOne() {
+    alert("Did you know Dr. James Andrews is a real person? My mom has heard of him.");
+}
+
+function moreTwo() {
+    alert("Way to go, TLAP!");
+}
+
+function moreThree() {
+    alert("Helping prevent surgery is important, so this app will do that.");
+}
